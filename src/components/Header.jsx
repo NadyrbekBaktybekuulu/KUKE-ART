2a848a332d5b4504b2846d2a842fd8fcd36498a1
Baktybek uulu Nadyrbek <b.taku-93@mail.ru>
2023-05-20 16:42:57 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <img className="logo" src='https://1000logos.net/wp-content/uploads/2016/10/Batman-Logo-1939.png' />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+const Profile = () => {
+    return (
+        <main className="main-content">
+            <div className="banner-wrapper">
+                <p><img className="banner-img" src="https://www.bannerbatterien.com/upload/filecache/Banner-Batterien-Windrder2-web_06b2d8d686e91925353ddf153da5d939.webp" /></p>
+            </div>
+
+            <div className="user-block">
+                <img className="user-avatar" src="https://www.hotelbooqi.com/wp-content/uploads/2021/12/128-1280406_view-user-icon-png-user-circle-icon-png.png" />
+
+                <div className="user-description">
+                    <p>lorem lorem lorem lorem lorem lorem</p>
+                </div>
+            </div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </main>
+    );
+}
+
+export default Profile;
