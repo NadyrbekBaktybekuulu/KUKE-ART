b06c8ff415d2dbdf7d3236e4b4a0a7f23b647206
Baktybek uulu Nadyrbek <b.taku-93@mail.ru>
2023-05-25 17:32:43 +0300

    Was deleted components folder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header/Header';
-import Nav from './components/Nav/Nav';
-import Profile from './components/Profile/Profile';
+
 
 
 
 const App = () => {
   return (
     <div className="app-wrapper">
-      <Header />
-      <Nav />
-      <Profile />
+      
     </div>
-
   );
 }
 
